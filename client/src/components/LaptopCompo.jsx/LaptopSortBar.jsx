651c0b4fd78379998d1edd39d725cf5e87754e91
Chhun Sivheng <sivmiosu@gmail.com>
2025-06-07 22:57:31 +0700

    LaptopPage

---
+++ client/src/components/LaptopCompo.jsx/LaptopSortBar.jsx
@@ -0,0 +1,17 @@
+export default function LaptopSortBar() {
+  return (
+    <div className="flex justify-between items-center mb-4">
+      <div>
+        <button className="bg-gray-100 px-3 py-1 rounded mr-2">
+          Sort By: Position
+        </button>
+        <button className="bg-gray-100 px-3 py-1 rounded">
+          Show: 35 per page
+        </button>
+      </div>
+      <div>
+        {/* Grid/List toggle icons can go here */}
+      </div>
+    </div>
+  );
+}
