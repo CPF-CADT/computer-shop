aed4a3017858b9f20f799cdaa8bc368682a1facb
Chhun Sivheng <sivmiosu@gmail.com>
2025-06-22 00:43:19 +0700

    ProductDetails Update on Review and Rearrange Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -21,18 +21,18 @@ function App() {
       <Head /> 
       <Nav />  
       <div className="pt-4 pb-8 px-4 md:px-6 lg:px-8 max-w-7xl mx-auto">
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/laptop" element={<Laptop />} />
-        <Route path="/desktop" element={<Desktop />} />
-        <Route path="/product/:productId" element={<ProductDetails />} />
-        <Route element={<CartLayout />}> 
-          <Route path="/cart" element={<ShoppingCartPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-        </Route>
-      </Routes>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+          <Route path="/laptop" element={<Laptop />} />
+          <Route path="/desktop" element={<Desktop />} />
+          <Route path="/product/:productId" element={<ProductDetails />} />
+          <Route element={<CartLayout />}> 
+            <Route path="/cart" element={<ShoppingCartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+          </Route>
+        </Routes>
       </div>
     </>
   );
