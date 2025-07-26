7bfbd5d56afa82f5d14f6dd837b9a4f0ed67cd3e
vathanakphy <vathanakphy@gmail.com>
2025-07-26 15:28:01 +0700

    update route security fro allow public for detail product

--- server/routes/product.route.ts
+++ server/routes/product.route.ts
@@ -1,15 +1,15 @@
-import { getAllProduct,getOneProduct,getProductDetail,addProductFeedback,addNewProduct,updateProduct, deleteProduct } from '../controller/product.controller';
+import { getAllProduct, getOneProduct, getProductDetail, addProductFeedback, addNewProduct, updateProduct, deleteProduct } from '../controller/product.controller';
 import express from 'express'
 import { authenticateToken, authorize } from '../middleware/authenticateToken.middleware';
 const productRouter = express.Router();
 
 productRouter.get('/', getAllProduct);
-productRouter.get('/:product_code',authenticateToken, getOneProduct);
-productRouter.get('/:product_code/detail',authenticateToken, getProductDetail);
-productRouter.post('/',authenticateToken,authorize('staff'), addNewProduct);
-productRouter.post('/:product_code/feedback',authenticateToken,authorize('staff'), addProductFeedback);
-productRouter.put('/:productCode',authenticateToken,authorize('staff'), updateProduct);
-productRouter.delete('/:productCode', authenticateToken,authorize('staff'), deleteProduct);
+productRouter.get('/:product_code', authenticateToken, getOneProduct);
+productRouter.get('/:product_code/detail', getProductDetail);
+productRouter.post('/', authenticateToken, authorize('staff'), addNewProduct);
+productRouter.post('/:product_code/feedback', authenticateToken, authorize('staff'), addProductFeedback);
+productRouter.put('/:productCode', authenticateToken, authorize('staff'), updateProduct);
+productRouter.delete('/:productCode', authenticateToken, authorize('staff'), deleteProduct);
 
 export default productRouter;
 
