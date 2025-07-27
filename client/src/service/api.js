4af18d7a30c35a5c9042f80af64c7a75b061b500
vathanakphy <vathanakphy@gmail.com>
2025-07-27 21:17:57 +0700

    Update api.js

--- client/src/service/api.js
+++ client/src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 const apiClient = axios.create({
-  // baseURL: 'https://computer-shop-4sqx.onrender.com/api/',
-  baseURL: 'http://localhost:3000/api/',
+  baseURL: 'https://computer-shop-4sqx.onrender.com/api/',
+  // baseURL: 'http://localhost:3000/api/',
   headers: {
     'Content-Type': 'application/json'
   }
